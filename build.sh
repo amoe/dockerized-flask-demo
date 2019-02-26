dcc9031a5ddc23922bb72c874a9510b9dd2d9984
David Banks <amoebae@gmail.com>
2019-02-26 16:11:34 +0000

    notes

--- build.sh
+++ build.sh
@@ -3,6 +3,32 @@ docker build -t myannotator_image .
 
 # Create container with specific name
 # Source path has to be absolute
+# Mem limits are specified at this time (create or run time) with
+# -m 5g
+
+# Env can be handled with this.
+# docker create -e MYVAR1 --env MYVAR2=foo --env-file ./env.list 
+# However env can also be hardcoded into the Dockerfile, at build time.
+# EXPOSE is the big one.
+
+# The EXPOSE instruction does not actually publish the port. It functions as a
+# type of documentation between the person who builds the image and the person who
+# runs the container, about which ports are intended to be published. To actually
+# publish the port when running the container, use the -p flag on docker run to
+# publish and map one or more ports, or the -P flag to publish all exposed ports
+# and map them to high-order ports.
+
+
+# docker run -p 127.0.0.1:80:8080/tcp ubuntu bash
+#  This binds port 8080 of the container to TCP port 80 on 127.0.0.1 of the host
+# > machine. You can also specify udp and sctp ports. The Docker User Guide explains
+# > in detail how to manipulate ports in Docker.
+
+# $ docker run --expose 80 ubuntu bash
+
+# This exposes port 80 of the container without publishing the port to the host
+# system’s interfaces.
+
 docker create --name myannotator_container \
   --mount type=bind,source=$(pwd)/code,destination=/code,readonly \
   myannotator_image
@@ -38,3 +64,13 @@ docker inspect myannotator_container
 #  * Detected change in '/code/app.py', reloading
 
 # Now you only need to rebuild if you change the infrastructure definitions.
+
+
+    # ports:
+    #  - "7005:8000"
+    # image: aus_lga_six_way_v2
+    # environment:
+    #  - TWO_FISHES_HOST=casm1.casmconsulting.co.uk
+    #  - TWO_FISHES_PORT=3033
+    #  - DEBUG_APP_VERBOSITY=0
+    # mem_limit: 4g
